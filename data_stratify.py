6a84e218c4ac3b6d22e3010cfd71aae476abba24
Tomer Efroni <t0merx.efr@gmail.com>
2022-05-06 12:41:06 +0300

    Implemented for one field

--- data_stratify.py
+++ data_stratify.py
@@ -6,24 +6,33 @@ from sklearn.model_selection import train_test_split
 
 
 def stratify_data(path, ratio, fields):
-    print("in stratify_data")
-    print(path, ratio, fields)
-    data = pd.read_csv(path, nrows=10)
+    data = pd.read_csv(path)
     headers = data.columns.to_list()
+    print("fields data before start:")
+    print(data['device'].value_counts())
     data = data.to_numpy()
-    print(data)
 
-    fieldsArgs = {"device":0, "location":1, "date":2, "os":3, "source":4, "sale":5,}
+    # Creating a dict which includes the fields & its index.
+    indexes = list(range(0, len(headers)))
+    fields_dict = dict(zip(headers, indexes))
 
     Y = []
     for el in data:
-        Y.append(el[fieldsArgs[fields[0]]])
+        Y.append(el[fields_dict[fields[0]]])
 
-    print(Y, "\n\n")
+    # print("\n", Y, "\n\n")
 
     X_train, X_test, y_train, y_test = train_test_split(data, Y, test_size=ratio, stratify=Y)
-    print("X_train:\n", X_train)
-    print("\nX_test:\n", X_test)
+    # print("X_train:\n", X_train)
+    # print("\nX_test:\n", X_test)
+
+    df_xtrain = pd.DataFrame(X_train)
+    df_xtest = pd.DataFrame(X_test)
+    print("\ndf_xtrain:")
+    print(df_xtrain[0].value_counts())
+
+    print("\ndf_xtest:")
+    print(df_xtest[0].value_counts())
 
 
 # Creating the arguments to get the parameters through CLI.
@@ -34,8 +43,8 @@ def parse_args():
     parser.add_argument('-fields', type=str, required=True, help="The fields of the data that the stratifying will be based on.\n Choose between: device, location, date, os, source, sale.")
     args = parser.parse_args()
 
-    isFile = os.path.isfile(args.path)
-    if not isFile:
+    is_file = os.path.isfile(args.path)
+    if not is_file:
         print("The file in the path that was entered is not exist. Please check the path and try again.")
         # Log here
         exit(1)
@@ -48,10 +57,10 @@ def parse_args():
     args_fields = args.fields.split(',')
     args_fields = [s.strip() for s in args_fields]
     fields = ["device", "location", "date", "os", "source", "sale"]
-    sharedArgs = [x for x in args_fields if x in fields]
+    shared_args = [x for x in args_fields if x in fields]
 
-    if not sharedArgs == args_fields:
-        print(sharedArgs, args_fields)
+    if not shared_args == args_fields:
+        print(shared_args, args_fields)
         print("Fields are incorrect. Please choose from the following:\ndevice, location, date, os, source, sale.")
         # log here
         exit(1)
