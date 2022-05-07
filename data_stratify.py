a2c2bd2a8675c855ef8c5ebc6b35792ba7bcf200
Tomer Efroni <t0merx.efr@gmail.com>
2022-05-07 21:49:08 +0300

    Finished logging + cosmetics

--- data_stratify.py
+++ data_stratify.py
@@ -1,33 +1,51 @@
 import pandas as pd
 import numpy as np
 import argparse, os, logging
+from datetime import datetime
 from sklearn.model_selection import train_test_split
 
 
+def init_log():
+    cwd = os.getcwd()
+    log_dir = os.path.join(cwd, "Logging_Files")
+    if not os.path.isdir(log_dir):
+        os.mkdir(log_dir)
+
+    try:
+        now = datetime.now()
+        log_fie = log_dir + "\Log " + now.strftime("%m.%d.%Y %H-%M-%S" + ".log") 
+        logging.basicConfig(filename=log_fie, filemode='w', level=logging.INFO, format='%(message)s')
+    except:
+        print("Could not create a log file.")
+        logging.basicConfig(level=logging.CRITICAL, format='%(message)s')
+
+################
 
 def stratify_data(path, ratio, fields):
-
     print("\nStarting splitting process..\n\n----------\n")
-    data = pd.read_csv(path).dropna() # Using dropna to remove rows with missing data.   nrows=10
+    logging.info("Starting splitting process..\n\n----------\n")
+    data = pd.read_csv(path).dropna() # Using dropna to remove rows with missing data.
     headers = data.columns.to_list()
 
     if ratio == 0 :
         print("All of the data was set to train. No data will go into test, therefore data_test.csv file won't be created.")
         data.to_csv("data_train.csv", index=False, header=headers)
-        # log here
+        logging.info("All of the data was set to train. No data will go into test, therefore data_test.csv file won't be created.\ndata_train.csv was successfully created.")
         return
     elif ratio == 1:
         print("All of the data was set to test. No data will go into train, therefore data_train.csv file won't be created.")
         data.to_csv("data_test.csv", index=False, header=headers)
-        # log here
+        logging.info("All of the data was set to test. No data will go into test, therefore data_train.csv file won't be created.\ndata_test.csv was successfully created.")
         return
 
-    print("Data before split:")
+    print("Data count per field to stratify before the split:\n")
+    logging.info("- Data count per field to stratify before the split -\n")
     for field in fields:
-        print(data[field].value_counts().rename_axis(field))
-        # log here
-    print("\n----------\n")
-    
+        print(data[field].value_counts().to_frame(field), "\n")
+        logging.info(f"{data[field].value_counts().to_frame(field)}\n")
+    print("----------\n")
+    logging.info("----------\n")
+
     data = data.to_numpy()
 
     # Creating a dict which includes the fields & its index.
@@ -47,24 +65,32 @@ def stratify_data(path, ratio, fields):
         Y.append(temp_list_item)
         temp_list_item = []
 
-    X_train, X_test, y_train, y_test = train_test_split(data, Y, test_size=ratio, stratify=Y)
+    try:
+        X_train, X_test, y_train, y_test = train_test_split(data, Y, test_size=ratio, stratify=Y)
+    except:
+        print("An error has occurred while splitting the data.\nThere might be too many fields to stratify versus the data that was provided.")
+        logging.error("Error:\nAn error has occurred while splitting the data.\nThere might be too many fields to stratify versus the data that was provided.")
+        return
 
     df_xtrain = pd.DataFrame(X_train)
     df_xtest = pd.DataFrame(X_test)
 
-    print("Train data with stratify:")
+    print("Train data count per field to stratify after the split :\n")
+    logging.info("- Train data count per field to stratify after the split -\n")
     for idx in field_idx:
-        print(df_xtrain[idx].value_counts(), "\n")
-        # log here
+        print(df_xtrain[idx].value_counts().to_frame(fields[idx]), "\n")
+        logging.info(f"{df_xtrain[idx].value_counts().to_frame(fields[idx])}\n")
 
-    print("Test data with stratify:")
+    print("Test data count per field to stratify after the split:")
+    logging.info("\n- Test data count per field to stratify after the split -\n")
     for idx in field_idx:
-        print(df_xtest[idx].value_counts(), "\n")
-        # log here
+        print(df_xtest[idx].value_counts().to_frame(fields[idx]), "\n")
+        logging.info(f"{df_xtest[idx].value_counts().to_frame(fields[idx])}\n")
 
     df_xtrain.to_csv("data_train.csv", index=False, header=headers)
     df_xtest.to_csv("data_test.csv", index=False, header=headers)
-    # log here
+    print("----------\n\ndata_train.csv was successfully created.\ndata_test.csv was successfully created.\n")
+    logging.info("----------\n\ndata_train.csv was successfully created.\ndata_test.csv was successfully created.")
 
 
 # Creating the arguments to get the parameters through CLI.
@@ -77,14 +103,10 @@ def parse_args():
 
     is_file = os.path.isfile(args.path)
     if not is_file:
-        print("Cannot open the file in this path. Please check the path/make sure to address the .csv file and try again.")
-        # Log here
-        exit(1)
+        raise Exception("Cannot open the file in this path. Please check the path/make sure to address the .csv file and try again.")
 
     if args.ratio > 1 or args.ratio < 0:
-        print("Ratio value is not correct! Please insert a number between 0 to 1.")
-        # log here
-        exit(1)
+        raise Exception("Ratio value is not correct! Please insert a number between 0 to 1.")
 
     args_fields = args.fields.split(',')
     args_fields = [s.strip() for s in args_fields]
@@ -92,19 +114,20 @@ def parse_args():
     shared_args = [x for x in args_fields if x in fields]
 
     if not shared_args == args_fields:
-        print("Fields are incorrect. Please choose from the following:\ndevice, location, date, os, source, sale.")
-        # log here
-        exit(1)
+        raise Exception("Fields are incorrect. Please choose from the following:\ndevice, location, date, os, source, sale.")
 
+    logging.info(f"The variables that were inserted:\nPath: {args.path}     Ratio: {args.ratio}     Fields: {args_fields}\n")
     return (args.path, args.ratio, args_fields)
 
+################
 
 if __name__ == '__main__':
     try:
-        # start logging
+        init_log()
         path, ratio, fields = parse_args()
         stratify_data(path, ratio, fields)
-    except:
-        print("An error has occurred. Please check log file for more information.")
-        # close log here
-
+    except Exception as e:
+        print(f"An error has occurred. Please check log file for more information:\n{e}")
+        logging.error(f"Error:\nAn error has occurred:\n{e}")
+    logging.shutdown()
+    print("Log file was successfully created. Program will shutdown now..")
